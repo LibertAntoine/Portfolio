7eb8c9a3b87cd94e611ca364c52eeabf972ec389
Antoine Libert <duncan-arrakis@hotmail.fr>
2020-10-13 15:12:05 +0200

    Add WebGL particule effect

---
+++ src/App.vue
@@ -0,0 +1,63 @@
+<template>
+  <div id="app">
+
+
+    <TitleBloc id="titleBloc"/>
+  </div>
+</template>
+
+<script>
+
+import TitleBloc from '@/components/TitleBloc.vue'
+
+export default {
+  name: 'App',
+  components: {
+    TitleBloc
+  }
+}
+</script>
+
+<style>
+
+
+body::-webkit-scrollbar {
+  display: none;
+}
+
+body {
+  -ms-overflow-style: none;  /* IE and Edge */
+  scrollbar-width: none;  /* Firefox */
+}
+
+
+
+body{
+  margin : 0px;
+  overflow: auto;
+}
+
+
+#Particules {
+  position: absolute;
+  top : 0%;
+  z-index : -2;
+}
+
+#titleBloc {
+
+  position: absolute;
+  top: 40%;
+  left: 45%;
+}
+
+#app {
+
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  
+}
+</style>
---
+++ src/components/TitleBloc.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="TitleBloc">
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TitleBloc'
+}
+
+
+/*
+Main
+#e3581f
+
+Traine
+#c02525
+#6c2e70
+#ffffff
+
+Back
+#090034
+
+
+Back Compétences 
+#792114
+
+
+
+Idée police
+Perpetua
+Adobe Arabic
+*/
+
+
+
+
+</script>
+
+
+<style scoped>
+h1, h2 {
+    color: #ffffff;
+}
+</style>
